e32004afd6d9f63ea0612100c25894c6fab43683
call-me-yohan <louis.marianne@umail.uom.ac.mu>
2025-02-27 14:17:37 +0400

    Add files via upload

---
+++ Classes/User.java
@@ -0,0 +1,43 @@
+package Classes;
+
+class User{
+	private String username;
+	private String password;
+	private String usertype;
+	
+	public User(String username, String password, String usertype) {
+		this.username = username;
+		this.password = password;
+		this.usertype = usertype;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getUsertype() {
+		return usertype;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+	
+	public boolean login(String username, String password) {
+		return (this.username.equals(username)&& this.password.equals(password));
+	}
+	
+}
+
